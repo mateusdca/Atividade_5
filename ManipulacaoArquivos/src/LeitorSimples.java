0a58b35c88ab209bcf72204b6f051f01b43c4910
Mateus Aguiar <mateusdca@gmail.com>
2025-05-27 11:02:10 -0300

    Add files via upload

---
+++ ManipulacaoArquivos/src/LeitorSimples.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeitorSimples {
+    public static void main(String[] args) {
+        try {
+            FileReader leitor = new FileReader("frase.txt");
+            BufferedReader buffer = new BufferedReader(leitor);
+
+            String linha;
+            while ((linha = buffer.readLine()) != null) {
+                System.out.println("frase lida: " + linha);
+            } 
+        buffer.close();
+        leitor.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo.");
+            e.printStackTrace();
+        }
+    }
+}
